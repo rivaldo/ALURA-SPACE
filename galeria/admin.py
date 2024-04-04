4b6a41f56b108146fa86d1cbe93ae16591fdc734
Rivaldo da Silva Junior <rivaldoj@see.pb>
2024-04-04 16:15:51 -0300

    primeiro commit

---
+++ galeria/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from galeria.models import Fotografia
+
+# Register your models here.
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda', 'publicada')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_filter = ('categoria', )
+    list_editable = ('publicada',)
+    list_per_page =1
+    
+admin.site.register(Fotografia, ListandoFotografias)
+
