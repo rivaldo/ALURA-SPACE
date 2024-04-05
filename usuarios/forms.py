93b1325d7a43c6c3121f2b4839bbebbed47b628f
Rivaldo da Silva Junior <rivaldoj@see.pb>
2024-04-05 14:55:53 -0300

    segundo commit

---
+++ usuarios/forms.py
@@ -0,0 +1,74 @@
+from django import forms
+
+class LoginForms (forms.Form):
+    nome_login = forms.CharField(
+        label='Nome de Login',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class' : 'form-control',
+                'placeholder' : 'Ex.: Rivaldo da Silva'
+            }
+        )
+    )
+    
+    senha = forms.CharField(
+        label= 'Digite sua senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class' : 'form-control',
+                'placeholder':'Digite sua senha'
+            }
+        )
+    )
+    
+class CadastroForm ( forms.Form):
+    nome_login = forms.CharField(
+        label='Nome de Cadastro',
+        required=True,
+        max_length=100,
+        widget=forms.TimeInput(
+            attrs={
+                'class' : 'form-control',
+                'placeholder' : 'Ex.: Rivaldo da Silva'
+            }
+        )
+    )
+    
+    email = forms.EmailField(
+        label='Endereço de e-mail',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class' : 'form-control',
+                'placeholder': 'Ex.: alguem@algumacoisa.com'
+            }            
+            
+        )
+    )
+    senha = forms.CharField(
+        label= 'Digite sua senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class' : 'form-control',
+                'placeholder':'Digite sua senha'
+            }
+        )
+    )
+    confirma_senha = forms.CharField(
+        label= 'Confirme a sua senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class' : 'form-control',
+                'placeholder':'Digite sua senha novamente'
+            }
+        )
+    )
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,55 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from usuarios.forms import LoginForms, CadastroForm
 # Create your views here.
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def login(request):
-    return render(request, 'usuarios/login.html')
+    form = LoginForms()
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+        
+        if form.is_valid():
+            nome = form['nome_login'].value()
+            senha = form['senha'].value()
+        
+        usuario = auth.authenticate(
+            request,
+            username=nome,
+            password=senha
+        )
+        
+        if usuario is not None:
+            auth.login(request, usuario)
+            return redirect('index')
+        else:
+            return redirect('login')
+    return render(request, 'usuarios/login.html', {'form':form})
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    form = CadastroForm()
+    
+    if request.method == 'POST':
+        form = CadastroForm(request.POST)
+        
+        
+        if form.is_valid():
+            if form['senha'].value() != form['cofirma_senha']:
+                return redirect('cadastro')
+            
+            nome = form['nome_cadastro'].value()      
+            email = form['email'].value()
+            senha = form['senha'].value()   
+            
+            if User.objects.filter(username=nome).exists():
+                return redirect(cadastro)
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            usuario.save()
+            return redirect('login')
+            
+    return render(request, 'usuarios/cadastro.html', {'form':form})
+
